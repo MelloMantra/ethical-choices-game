aa1762fd66788f779c65b85492735cfa19ed0584
Ripley Bobko <rbobko08@gmail.com>
2024-12-22 15:17:21 -0700

    add world and door w/out script

--- ethical-choices-game/Player/player_main.gd
+++ ethical-choices-game/Player/player_main.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 2
-
+const accel = 4 #accel amount / sec
 
 @onready var camera = $Camera3D
 
@@ -20,8 +20,8 @@ func _physics_process(delta):
 	camDirection.basis.y = Vector3.UP
 	var direction = (camDirection.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = min(abs(direction.x * SPEED), abs(velocity.x + direction.x * accel * delta)) * sign(direction.x)
+		velocity.z = min(abs(direction.z * SPEED), abs(velocity.z + direction.z * accel * delta)) * sign(direction.z)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
