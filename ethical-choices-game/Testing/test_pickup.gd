a8d43174fcfd5ad97db3f53fced9d68ece9d6402
Ripley Bobko <rbobko08@gmail.com>
2025-01-12 21:31:36 -0700

    fix smol bug

--- ethical-choices-game/Testing/test_pickup.gd
+++ ethical-choices-game/Testing/test_pickup.gd
@@ -9,21 +9,27 @@ func _ready():
 	connect("mouse_entered", mouse_hover)
 	connect("mouse_exited", mouse_unhover)
 
+var showingMsg : bool = false
 
 func mouse_hover():
 	mouseHover = true
 	print("mouse hovered ", global_position.distance_to(player.global_position))
 	if global_position.distance_to(player.global_position) < 2:
 		BasicClassFunctions.spawnPrompt(str('Press "E" to pick up', visible_name), self)
+		showingMsg = true
 func mouse_unhover():
 	mouseHover = false
-	
+	showingMsg = false
 	BasicClassFunctions.removePrompt(self)
 	
 
 func _process(delta):
 	if global_position.distance_to(player.global_position) > 2 and mouseHover:
 		BasicClassFunctions.removePrompt(self)
+		showingMsg = false
+	if global_position.distance_to(player.global_position) < 2 and mouseHover and !showingMsg:
+		BasicClassFunctions.spawnPrompt(str('Press "E" to pick up', visible_name), self)
+		showingMsg = true
 
 func _input(event):
 	if Input.is_action_just_pressed("interact") and mouseHover:
